3da2761edd90fc76b5a5e41cadd92eaae8442e70
Gusta <Gusta@DESKTOP-MSFL281>
2023-04-06 07:54:47 -0600

    Initial Commitment

--- src/Project1/Project1.java
+++ src/Project1/Project1.java
@@ -1,5 +1,26 @@
 package Project1;
 
 public class Project1 {
+	
+	public static void main(String[] args) {
+		
+	String Result = reverse("HELLO"); 
+		
+	System.out.println(Result);
+		
+	}
 
+	
+	
+	public static String reverse(String str) { 
+		
+		StringBuilder Reverse = new StringBuilder(str); 
+		
+		return Reverse.reverse().toString(); 
+		
+		
+		
+	}
+	
+	
 }
